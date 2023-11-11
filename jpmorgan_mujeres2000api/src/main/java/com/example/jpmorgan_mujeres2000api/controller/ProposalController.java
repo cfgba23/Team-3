a340411038741cdbf3ecd50044c5ddabc6ff68ef
juliojalvare <juliojoel.alvarez@mercadolibre.com>
2023-11-11 19:38:40 -0300

    Adding documentation

--- jpmorgan_mujeres2000api/src/main/java/com/example/jpmorgan_mujeres2000api/controller/ProposalController.java
+++ jpmorgan_mujeres2000api/src/main/java/com/example/jpmorgan_mujeres2000api/controller/ProposalController.java
@@ -1,8 +1,10 @@
 package com.example.jpmorgan_mujeres2000api.controller;
 
 import com.example.jpmorgan_mujeres2000api.dto.ProposalDTO;
-import com.example.jpmorgan_mujeres2000api.entity.Proposal;
 import com.example.jpmorgan_mujeres2000api.services.ProposalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,27 +13,48 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/")
+@Tag(name = "Proposal", description = "Api to manage proposals")
 public class ProposalController {
 
     private final ProposalService proposalService;
 
+    @Operation(summary = "Get all proposals")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Found the proposals"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Proposal not found")
+    })
     @GetMapping("/proposal/{id}")
     ResponseEntity<Object> getProposal(@PathVariable Long id){
         return new ResponseEntity<>(proposalService.getByProposalId(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Create a new proposal")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "Proposal created"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Proposal not found")
+    })
     @PostMapping("/proposal")
     ResponseEntity<Object> createProposal(@RequestBody ProposalDTO proposal){
         proposalService.createProposal(proposal);
         return new ResponseEntity<>("Proposal created", HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a proposal")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Proposal updated"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Proposal not found")
+    })
     @PutMapping("/proposal/update/{id}")
     ResponseEntity<Object> updateProposal(@PathVariable Long id, @RequestBody ProposalDTO proposal){
         proposalService.updateProposal(id, proposal);
         return new ResponseEntity<>("Proposal updated", HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a proposal")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Proposal deleted"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "Proposal not found")
+    })
     @DeleteMapping("/proposal/delete/{id}")
     ResponseEntity<Object> deleteProposal(@PathVariable Long id){
         proposalService.deleteProposal(id);
--- jpmorgan_mujeres2000api/src/main/java/com/example/jpmorgan_mujeres2000api/controller/UserController.java
+++ jpmorgan_mujeres2000api/src/main/java/com/example/jpmorgan_mujeres2000api/controller/UserController.java
@@ -5,6 +5,10 @@ package com.example.jpmorgan_mujeres2000api.controller;
 import com.example.jpmorgan_mujeres2000api.entity.Proposal;
 import com.example.jpmorgan_mujeres2000api.entity.User;
 import com.example.jpmorgan_mujeres2000api.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +19,15 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/")
+@Tag(name = "User", description = "Api to manage users")
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "Get all users")
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Found the users"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "404", description = "User not found")
+    })
     @GetMapping("/user/{id}")
     ResponseEntity<Object> getUser(@PathVariable Long id){
         User user = userService.getById(id);
@@ -26,12 +36,17 @@ public class UserController {
         }
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
+    @Operation(summary = "Create a new user")
+    @ApiResponse(responseCode = "201", description = "User created")
     @PostMapping("/user")
     ResponseEntity<Object> createUser(@RequestBody User user){
         userService.createUser(user);
         return new ResponseEntity<>("User created", HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a user")
+    @ApiResponse(responseCode = "200", description = "User updated")
     @DeleteMapping("/user/delete/{id}")
     ResponseEntity<Object> deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
